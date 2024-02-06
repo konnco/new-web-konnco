c04c2e86160126275cdf6d4ef506fb4ab4f5a919
Jonathan Billy <jonathan.billy@ti.ukdw.ac.id>
2024-02-06 13:16:17 +0700

    style typo pada judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/konnco-logo.png') }}">
-    <title>Konnco Studio - Excetue Better</title>
+    <title>Konnco Studio - Execute Better</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <script src="https://cdn.tailwindcss.com"></script>
